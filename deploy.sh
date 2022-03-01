4ec87858e462585302b875f95855a8d63da99458
Rohith99123 <100128793+Rohith99123@users.noreply.github.com>
2022-03-01 10:56:59 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ so the file name is deploy
 because the file which is used in the deployment 
 it is known as deploy file
 i dont know what to type i am just 
-typing something to type
+typing something to typ
 
+the new one .. for merge
